a3b11e22d04fb2598e79582de6d01127da2b5a2e
6Morpheus6 <wanton-decay@gmx.net>
2025-03-07 07:01:01 +0100

    script version 3.0, add reset and update button

--- pinokio.js
+++ pinokio.js
@@ -1,40 +1,87 @@
+const path = require('path')
 module.exports = {
+  version: "3.0",
   title: "MagicAnimate",
   icon: "icon.gif",
-  description: "[NVIDIA GPU Only] Temporally Consistent Human Image Animation using Diffusion Model https://showlab.github.io/magicanimate/",
-  menu: async (kernel) => {
-    let installed = await kernel.exists(__dirname, "env")
-    if (installed) {
-      let session = await kernel.require(__dirname, "session.json")
-      let running = await kernel.running(__dirname, "start.json")
-      if (running) {
+  description: "[NVIDIA ONLY] Temporally Consistent Human Image Animation using Diffusion Model https://showlab.github.io/magicanimate/",
+  menu: async (kernel,info) => {
+    let installed = info.exists("app/env")
+    let running = {
+      install: info.running("install.json"),
+      start: info.running("start.json"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js")
+    }
+    if (running.install) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.json",
+      }]
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.json")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.json",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.json",
+          }]
+        }
+      } else if (running.update) {
         return [{
-          icon: "fa-solid fa-spin fa-circle-notch",
-          text: "Running"
-        }, {
-          icon: "fa-solid fa-rocket",
-          text: "Open UI",
-          href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
-          target: "_blank"
-        }, {
-          icon: "fa-solid fa-terminal",
-          text: "Terminal",
-          href: "start.json"
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Updating",
+          href: "update.js",
+        }]
+      } else if (running.reset) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Resetting",
+          href: "reset.js",
         }]
       } else {
         return [{
+          default: true,
           icon: "fa-solid fa-power-off",
-          text: "Launch",
+          text: "Start",
           href: "start.json",
-          params: { fullscreen: true, run: true, mode: "single" }
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.json",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+          confirm: "Are you sure you wish to reset the app?"
         }]
       }
     } else {
       return [{
+        default: true,
         icon: "fa-solid fa-plug",
         text: "Install",
         href: "install.json",
-        params: { run: true, fullscreen: true }
       }]
     }
   }
